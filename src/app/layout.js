ed1b540316141d06d9885ddde754a77f6008c57a
Aditya Hongal <adityahongalwork@gmail.com>
2025-03-18 17:51:56 +0530

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { ThemeProvider } from '../components/ThemeContext';
+import { Analytics } from "@vercel/analytics/react"
 import './globals.css';
 
 export const metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
           <Navbar />
           {children}
           <Footer />
+          <Analytics />
         </body>
       </html>
     </ThemeProvider>
